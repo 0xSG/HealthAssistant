273965010aa7b9de0c76a85d3b6bcb23fe405176
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-03-01 22:28:02 +0530

    selection lib added

--- app/src/main/java/com/atomtray/android/healthassistant/MainActivity.kt
+++ app/src/main/java/com/atomtray/android/healthassistant/MainActivity.kt
@@ -2,11 +2,18 @@ package com.atomtray.android.healthassistant
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chip_cloud.addChip("yo ")
+        chip_cloud.addChip("yo ")
+        chip_cloud.addChip("yo ")
+        chip_cloud.addChip("yo ")
+
+
     }
 }
