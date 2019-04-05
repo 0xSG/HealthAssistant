fe62b53c6b813995dd0c85d9cf8e2a448b98c45b
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-04-05 20:13:31 +0530

    New activity created.

---
+++ app/src/main/java/com/atomtray/android/healthassistant/ResultScreen.java
@@ -0,0 +1,13 @@
+package com.atomtray.android.healthassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_screen_activity);
+    }
+}
